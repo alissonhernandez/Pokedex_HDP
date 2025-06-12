f46d6cdf8de02147ead695dacfc462fcb3c833a1
CesarZV23005 <zv23005@ues.edu.sv>
2025-06-12 11:34:06 -0600

    update dentro de pokemonService para la obtencion de los pokemon

---
+++ javaScript/pokemonService.js
@@ -0,0 +1,21 @@
+import { Pokemon } from './pokemon.js';
+
+//obentencion de los primeros 150 pokemon
+export async function obtenerPokemones(limit = 150) {
+  const res = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}`);
+  const data = await res.json();
+
+  const pokemones = await Promise.all(
+    data.results.map(async p => {
+      const detalles = await fetch(p.url).then(r => r.json());
+
+      //obtencion de debilidades desde el primer tipo
+      const tipoURL = detalles.types[0].type.url;
+      const tipoDetalles = await fetch(tipoURL).then(r => r.json());
+
+      return new Pokemon(detalles, tipoDetalles);
+    })
+  );
+
+  return pokemones;
+}
