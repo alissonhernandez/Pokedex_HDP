07d9da101ab1c1b81294bb50fe57cca515ea8823
CesarZV23005 <zv23005@ues.edu.sv>
2025-06-21 14:10:43 -0600

    update muestraTarjeta.js muestra del abanico

--- javaScript/muestraTarjeta.js
+++ javaScript/muestraTarjeta.js
@@ -16,8 +16,13 @@ export async function mostrarTarjetasBreves(limit = 150) {
     }
   });
 
-  tiposUnicos.forEach(pokemon => {
-    const card = muestratarjeta(pokemon);
+  // Convertimos el Map a array y aplicamos índice centrado
+  const pokes = Array.from(tiposUnicos.values());
+  const middle = Math.floor(pokes.length / 2);
+
+  pokes.forEach((pokemon, i) => {
+    const offset = i - middle; // genera valores simétricos: -3, -2, ..., 0, ..., 2, 3
+    const card = muestratarjeta(pokemon, offset);
     contenedor.appendChild(card);
   });
 }
